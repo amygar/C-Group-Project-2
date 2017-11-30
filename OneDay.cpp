f930ef4634c603b5c711cf4c9a515e8b1f0f672c
amygar <34141173+amygar@users.noreply.github.com>
2017-11-30 15:13:57 -0500

    Add files via upload

---
+++ OneDay.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int choice;
+
+    //Before game starts
+    cout << "Welcome to One Day! Type the following number to:" << endl;
+    cout << "1. Start Game." << endl << "2. Credits. " << endl "3. Exit.";
+
+    //Introduction
+    cout << "You hear a faint sound amongst the darkness. The sound doesn’t go away and starts to grow even louder. Unable to ignore it for another minute, you find yourself becoming more aware." << endl
+    << "You realize that the sound is coming from your alarm clock. It’s Friday and you must go to school   you go on with your daily morning routine where you get ready and you eat breakfast." << endl
+    << "Once done you begin to decide how you want to go about your day after all you can’t just sit around your room all day. "
+
+    <<"1. Go to school and attend classes." <<endl;
+    <<"2. Take a walk and rethink life." <<endl;
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1:
+        {
+            cout <<"You decide to go to school after all. Might as well act responsibly like an adult." << endl
+                << "As you go from class to class the day seems to get shorter. It is 2:00 pm and you have one final class." << endl
+                << "As you make your way to your last class you see a small group in the distance. The groups turns out to be four boys.\n Three are kicking the life out of one of them.\n\n What do you do?\n";
+                break;
+        }
+        case 2:
+        {
+            cout <<"It seems to you that it is time to rethink your life. Missing class is the least of your worries." << endl
+            << "As you walk for hours you get to cross a bridge. At the middle of that bridge there is a young woman.\n This woman is getting ready to attempt suicide.\n\n What’s your first instinct?";
+            break;
+        }
+    }
+
+}
